592af0e83b4d95bf9dfb69510136970afbd229bf
Felipe Abdo <felipe.abdo.ti@gmail.com>
2023-10-11 20:47:49 -0300

    Exercicio 1 pronto para revisão

---
+++ dist/ex-1.js
@@ -0,0 +1,28 @@
+"use strict";
+// 1 - Criar uma função que retorne a quantidade de vogais da palavra passada.
+// a) Dar um exemplo de uso com uma palavra recebida via parâmetro da função.
+// b) Dar um exemplo de uso com uma palavra recebida via input no formulário.
+// Exemplo via função:
+function contarVogais(palavra) {
+    const vogais = "aeiouAEIOUáàéíóúãõâêôäëïöü";
+    let contar = 0;
+    for (let i = 0; i < palavra.length; i++) {
+        if (vogais.includes(palavra[i])) {
+            contar++;
+        }
+    }
+    return contar;
+}
+console.log(contarVogais('Felipe'));
+// Exemplo de uso com uma palavra recebida via input no formulário
+const formulario = document.getElementById("formulario");
+formulario.addEventListener("submit", function (event) {
+    event.preventDefault();
+    const inputPalavra = document.getElementById("palavra");
+    const palavra = inputPalavra.value;
+    const resultadoElement = document.querySelector("p");
+    if (resultadoElement) {
+        resultadoElement.innerHTML = `O número de vogais de "${palavra.toUpperCase()}" é ${contarVogais(palavra)}!`;
+    }
+});
+//# sourceMappingURL=ex-1.js.map
