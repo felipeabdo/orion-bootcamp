0dc025e2aaef146f5efd7550e8cb9e3b96dc526b
Felipe Barroso Abdo <felipe.abdo@gmail.com>
2023-10-13 09:15:48 -0300

    Faltou um ;

--- exercício-2/src/index.ts
+++ exercício-2/src/index.ts
@@ -9,7 +9,7 @@ let lista: Person[] = [
   {"id" : 2, "name": "Alan Turing", "bio" : "Alan Turing foi um matemático, cientista da computação, lógico, criptoanalista, filósofo e biólogo teórico britânico, ele é amplamente considerado o pai da ciência da computação teórica e da inteligência artificial"},
   {"id" : 3, "name": "Nikola Tesla", "bio" : "Nikola Tesla foi um inventor, engenheiro eletrotécnico e engenheiro mecânico sérvio, mais conhecido por suas contribuições ao projeto do moderno sistema de fornecimento de eletricidade em corrente alternada."},
   {"id" : 4, "name": "Nicolau Copérnico", "bio": "Nicolau Copérnico foi um astrônomo e matemático polonês que desenvolveu a teoria heliocêntrica do Sistema Solar"}
-]
+];
 
 // FUNÇÕES NO PARADIGMA FUNCIONAL
 
