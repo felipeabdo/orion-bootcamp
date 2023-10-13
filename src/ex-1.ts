daafe83e5787236a289223b91e5f535bbd72e42f
Felipe Barroso Abdo <felipe.abdo@gmail.com>
2023-10-13 19:23:03 -0300

    Desafio revisado

--- src/ex-1.ts
+++ src/ex-1.ts
@@ -1,36 +1,51 @@
-// 1 - Criar uma função que retorne a quantidade de vogais da palavra passada.
-// a) Dar um exemplo de uso com uma palavra recebida via parâmetro da função.
-// b) Dar um exemplo de uso com uma palavra recebida via input no formulário.
+/**
+ * Função que conta a quantidade de vogais em uma palavra.
+ * @param palavra - A palavra da qual você deseja contar as vogais.
+ * @returns O número de vogais na palavra.
+ * @example
+ * const palavra = "Araraquara";
+ * const resultado = contagem(palavra);
+ * console.log(`O número de vogais de ${palavra} é ${resultado}!`);
+ */
 
-// Exemplo via função:
+function contagem(palavra: string): number {
 
-function contarVogais(palavra: string): number {
-  const vogais = "aeiouAEIOUáàéíóúãõâêôäëïöü";
+  const vogais = "aeiouáàéíóúãõâêôäëïöü";
+
   let contar = 0;
 
   for (let i = 0; i < palavra.length; i++) {
-    if (vogais.includes(palavra[i])) {
+    if (vogais.includes(palavra[i].toLowerCase())) {
       contar++;
     }
   }
 
   return contar;
+
 }
 
-console.log(contarVogais('Felipe'))
+let palavra = "Araraquara"
 
-// Exemplo de uso com uma palavra recebida via input no formulário
+console.log(`O número de vogais de ${palavra} é ${contagem(palavra)}!`)
+
+/**
+ * Manipula o envio do formulário para contar as vogais da palavra inserida e exibir o resultado.
+ */
 
 const formulario = document.getElementById("formulario") as HTMLFormElement;
 
 formulario.addEventListener("submit", function (event) {
 
   event.preventDefault();
+
   const inputPalavra = document.getElementById("palavra") as HTMLInputElement;
+
   const palavra = inputPalavra.value;
-  const resultadoElement = document.querySelector("p");
-  if (resultadoElement) {
-    resultadoElement.innerHTML = `O número de vogais de "${palavra.toUpperCase()}" é ${contarVogais(palavra)}!`;
+
+  const resultadoElemento = document.querySelector("p");
+
+  if (resultadoElemento) {
+    resultadoElemento.innerHTML = `O número de vogais de "${palavra.toUpperCase()}" é ${contagem(palavra)}!`;
   }
 
 });
